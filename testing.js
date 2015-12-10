f88dccf577555decbfb54acd90f7311e0e410adb
sourbh-daffodilsw <sourbh.gupta@daffodilsw.com>
2015-12-10 14:17:31 +0530

    ok

---
+++ testing.js
@@ -0,0 +1,11 @@
+var sum = require("privatemodule2");
+var Q = require("q");
+
+var print = function(a,b){
+var d = Q.defer();
+console.log("a >>>>>>"+a);
+console.log("b >>>>>>"+b);
+console.log("a+b >>>>"+sum(a,b));
+d.resovle();
+return d.promise;
+}
